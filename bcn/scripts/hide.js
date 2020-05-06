f07df4a488b807a9b8c791c72f83c2d6b8adbd0e
jotaemedois <jotaemedois@gmail.com>
2020-05-06 19:16:27 +0100

    only hide if one video is loaded

--- bcn/scripts/hide.js
+++ bcn/scripts/hide.js
@@ -1,5 +1,12 @@
+var myVid = document.getElementById("loadTest");
+var loaded = false;
+myVid.onloadeddata = function() {
+	console.log("Browser has loaded the current frame");
+    loaded = true;
+}
+
 $(document).ready(function() {
     $("#hide").click(function() {
-        $(".overlay").hide();
+        if (loaded == true) {$(".overlay").hide();}
     });
 });
